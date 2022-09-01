92a40395863df2d4f3fae80abf93addd9cd9c083
Al <alpalma95@gmail.com>
2022-09-01 17:10:40 +0200

    deleted server.js and unified logic in app

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+const DB = process.env.DATABASE;
 
 const globalErrorHandler = require('./controllers/errorController');
 const userRouter = require('./routes/userRoutes');
 const todoRouter = require('./routes/todoRoutes');
 
 const app = express();
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB connection successful!'))
+  .catch((err) => console.log(err));
 
 // 1) MIDDLEWARES
 
@@ -18,4 +32,7 @@ app.use('/api/v1/todos', todoRouter);
 app.use(globalErrorHandler); // This is handling all the errors we throw from the next() on the middlewares, as well as the catch blocks.
 
 // START THE SERVER
-module.exports = app;
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log('App running on port 3000');
+});
